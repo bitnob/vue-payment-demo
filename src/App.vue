2394f188dc5241cb91285c85e3b0d27484494240
gabbyprecious <gabbyprecious2000@gmail.com>
2022-08-08 14:12:42 +0100

    adding bitnob payment button to project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <section>
+      <h1>Pay with Bitnob</h1>
+      <div class="form-container">
+        <hr />
+        <div>
+          <div class="input-div">
+            <label for="email"><b>Email</b></label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              id="email"
+              required
+            />
+          </div>
+          <div class="input-div">
+            <label for="amount"><b>Amount</b></label>
+            <input
+              type="number"
+              placeholder="Enter amount in Naira"
+              name="amount"
+              id="amount"
+              required
+            />
+          </div>
+          <bitnob
+            :amount="amount"
+            :currency="currency"
+            :customerEmail="email"
+            :publicKey="publicKey"
+            :reference="reference"
+            :environment="environment"
+            :notificationEmail="notificationEmail"
+          >
+            Make Payment
+          </bitnob>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
 
-export default {
-  name: 'App',
+
+import { Bitnob } from "vue-bitnob";
+
+export default Vue.extend({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Bitnob,
+  },
+  data() {
+    return {
+      publicKey: "pk.9058f.0a5cb70de0f5853b9febfa",
+      email: "foobar@example.com",
+      amount: 10000,
+      currency: "NGN",
+      notificationEmail: "gabbyprecious2000@gmail.com",
+      environment: "sandbox",
+    };
+  },
+  computed: {
+    reference() {
+      let text = "";
+      let possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for (let i = 0; i < 10; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+    },
+  },
+});
 </script>
 
 <style>
@@ -24,5 +86,69 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  overflow-x: hidden;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+
+.container {
+  padding: 3rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  /* height: 100%;
+            width: 100%; */
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+}
+
+.form-container div {
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.form-container input {
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 15px;
+  margin-bottom: 10px;
+  width: 75%;
+}
+
+.input-div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+input {
+  box-sizing: border-box;
+
+  background: #ffffff;
+  border: 1px solid rgba(153, 163, 160, 0.6);
+  border-radius: 16px;
+}
+
+p {
+  font-size: 14px;
+  font-weight: 900;
 }
 </style>
